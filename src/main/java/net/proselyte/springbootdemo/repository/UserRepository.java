f518e45cb87c3dbe27d81536c73832dbeb10c8f5
AlexeiBaza <AlexeiBazaGM@gmail.com>
2022-05-26 16:31:44 +0300

    init commit

---
+++ src/main/java/net/proselyte/springbootdemo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package net.proselyte.springbootdemo.repository;
+
+import net.proselyte.springbootdemo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//JpaRepository - некий интерфейс который с помощью Reflection API способен генерировать запросы
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
