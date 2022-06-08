12aa590f7d15451985d4ca502ce56709e9a8114e
AlexeiBaza <AlexeiBazaGM@gmail.com>
2022-06-08 18:16:38 +0300

    start Spring Security

---
+++ src/main/java/net/proselyte/springbootdemo/config/MvcConfig.java
@@ -0,0 +1,14 @@
+package net.proselyte.springbootdemo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//по умолчанию springSecurity предоставляет системную авторизацию.
+//Если хотим свою то добавляем представление login
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/user").setViewName("users");
+    }
+}
---
+++ src/main/java/net/proselyte/springbootdemo/config/WebSecurityConfig.java
@@ -0,0 +1,59 @@
+package net.proselyte.springbootdemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final SuccessUserHandler successUserHandler;
+
+    public WebSecurityConfig(SuccessUserHandler successUserHandler) {
+        this.successUserHandler = successUserHandler;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                    .authorizeRequests()
+
+                    //Доступ разрешен всем пользователей
+                    .antMatchers("/").permitAll()
+
+                    //Все остальные страницы требуют аутентификации
+                    .anyRequest().authenticated()
+
+                .and()
+
+                    //Настройка для входа в систему
+                    .formLogin()
+
+                    //Перенарпавление на страницы после успешного входа согласно successUserHandler
+                    .successHandler(successUserHandler)
+                    .permitAll()
+                .and()
+                    .logout()
+                    .permitAll();
+    }
+
+    // аутентификация inMemory
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("u")
+                        .password("p")
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
