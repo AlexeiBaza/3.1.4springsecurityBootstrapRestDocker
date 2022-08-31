77c5c972a4c7404888b1d87a4f2268cf10a503f6
Aleksey Simukov <AlexeiBazaGM@gmail.com>
2022-08-31 21:47:18 +0300

    All work but without users

--- src/main/java/net/proselyte/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/net/proselyte/springbootdemo/SpringBootDemoApplication.java
@@ -1,18 +1,9 @@
 package net.proselyte.springbootdemo;
 
-import net.proselyte.springbootdemo.model.Role;
-import net.proselyte.springbootdemo.model.User;
 import net.proselyte.springbootdemo.repository.UserRepository;
-import net.proselyte.springbootdemo.service.RoleService;
-import net.proselyte.springbootdemo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.util.*;
 
 @SpringBootApplication
 public class SpringBootDemoApplication {
@@ -31,11 +22,10 @@ public class SpringBootDemoApplication {
 //			Role roleAdmin = new Role("ADMIN");
 //			Role roleUser = new Role("USER");
 //
-//			User admin1 = new User("admin1", "admin1", 111, "admin1@mail", "admin1", Arrays.asList(roleAdmin, roleUser));
-//			User admin2 = new User("admin2", "admin2", 222, "admin2@mail", "admin2", Collections.singletonList(roleAdmin));
-//			User user1 = new User("user1", "user1", 11, "user1@mail", "user1", Collections.singletonList(roleUser));
-//			User user2 = new User("user2", "user2", 22, "user2@mail", "user2", Collections.singletonList(roleUser));
-//			User user3 = new User("user3", "user3", 33, "user3@mail", "user3", Collections.singletonList(roleUser));
+//			User admin1 = new User("admin1", "admin1", 10, "admin1@mail", "admin1", Arrays.asList(roleAdmin, roleUser));
+//			User admin2 = new User("admin2", "admin2", 20, "admin2@mail", "admin2", Collections.singletonList(roleAdmin));
+//			User user1 = new User("user1", "user1", 10, "user1@mail", "user1", Collections.singletonList(roleUser));
+//			User user2 = new User("user2", "user2", 20, "user2@mail", "user2", Collections.singletonList(roleUser));
 //
 ////			For H2 database
 ////			roleService.create(roleAdmin);
@@ -45,7 +35,6 @@ public class SpringBootDemoApplication {
 //			userService.create(admin2);
 //			userService.create(user1);
 //			userService.create(user2);
-//			userService.create(user3);
 //		};
 //	}
 
